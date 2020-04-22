b978c663b56dc898750b4a46a752ab4694afdf9f
Chloe Montgomery <chloemontgomery01@gmail.com>
2020-04-22 14:24:53 +0100

    Begin to all file upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,25 +27,47 @@ const Spacer = styled.div`
   height: 50px;
 `;
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <TwoThirdsWidth>
-      <h1>Taking some of the stress out of healthcare rotas</h1>
-      <p>
-        A tool that helps healthcare workers get a simple version of their work
-        schedule. Knowing when you are working should be easy.
-      </p>
-      <SubmitButton>Upload from your computer</SubmitButton>
+const IndexPage = () => {
 
-      <Link to="/setup/">Setup page</Link>
-      <Spacer />
-      <p>
-        This is an open-source project. If you think you can help, get in touch
-        or head over to the GitHub page.
-      </p>
-    </TwoThirdsWidth>
-  </Layout>
-);
+  const onChangeHandler = (e) => {
+    const file = e.target.files[0];
+    const data = new FormData();
+    data.append('file', file);
+
+    fetch('http://127.0.0.1:5000/parse', {
+      method: 'POST',
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+      mode: 'no-cors',
+      body: data
+    })
+
+  };
+
+  return (
+      <Layout>
+        <SEO title="Home"/>
+        <TwoThirdsWidth>
+          <h1>Taking some of the stress out of healthcare rotas</h1>
+          <p>
+            A tool that helps healthcare workers get a simple version of their work
+            schedule. Knowing when you are working should be easy.
+          </p>
+          <SubmitButton>Upload from your computer</SubmitButton>
+
+          <label htmlFor="myfile">Select a file:</label>
+          <input type="file" id="myfile" name="myfile" onChange={onChangeHandler}/>
+
+          <Link to="/setup/">Setup page</Link>
+          <Spacer/>
+          <p>
+            This is an open-source project. If you think you can help, get in touch
+            or head over to the GitHub page.
+          </p>
+        </TwoThirdsWidth>
+      </Layout>
+  )
+};
 
 export default IndexPage;
