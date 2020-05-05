6fefe2f74c66b250a5a20706d9711698bf08bddb
nawhi <25394965+nawhi@users.noreply.github.com>
2020-05-05 18:04:55 +0100

    Inject calendar data to schedule page via sessionStorage

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,17 +1,30 @@
-import React, {useState} from "react";
+import React from "react";
 import {Link} from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import CalendarComponent from "../components/calendar";
-import {SelectFile} from "../components/selectFile";
+import ErrorPage from "../components/errorPage";
 
-const Schedule = ({ calendarData, updateCalendarData }) => {
+function retrieveCalendarData() {
+    try {
+        return JSON.parse(atob(sessionStorage.getItem("simplerotas")!!));
+    } catch (e) {
+        return null;
+    }
+}
+
+const Schedule: React.FunctionComponent = () => {
+    const calendarData = retrieveCalendarData();
+    if (!calendarData) {
+        return <ErrorPage/>;
+    }
+
+    console.log({calendarData});
     return (
         <Layout>
             <SEO title="Schedule"/>
             <h2>Your schedule</h2>
-            {/*<SelectFile updateCalendarData={updateCalendarData}/>*/}
             <CalendarComponent calendarData={calendarData}/>
             <Link to="/">Go back to the homepage</Link>
         </Layout>
